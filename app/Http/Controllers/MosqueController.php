4b19521ee2e8e2059b17baf93bc6da5698a08900
mfatih5426 <gagas112212@gmail.com>
2023-05-08 13:46:25 +0700

    setup projek dan model

---
+++ app/Http/Controllers/MosqueController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mosque;
+use Illuminate\Http\Request;
+
+class MosqueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Mosque $mosque)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Mosque $mosque)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Mosque $mosque)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Mosque $mosque)
+    {
+        //
+    }
+}
---
+++ app/Models/Mosque.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mosque extends Model
+{
+    use HasFactory;
+    protected $fillable = ['user_id','address','regency_id','province_id','coordinator','mosque_account_number','bmi_account_number','history'];
+
+    public function user(){
+        return $this->belongsTo(User::class,);
+    }
+}
